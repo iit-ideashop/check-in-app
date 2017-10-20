1eee7ea2eaec35ca3ae0559319cb9e11d843f2c7
ipro <ipro@iit.edu>
2017-10-20 18:15:47 -0500

    websocket javascript listen

--- checkIn/static/js/checkin.js
+++ checkIn/static/js/checkin.js
@@ -1,2 +1,10 @@
-$(function() {
-})
+$(function() 
+var socket = new io.Socket('ideashop-fedora',
+	{port: 5000});
+socket.connect();
+
+socket.on('message',function(data)
+	{window.location.href = data;}
+);
+)
+

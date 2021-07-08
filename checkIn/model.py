a0217e0b9a9f0f59dfab23c279ac954029414d67
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2021-07-08 11:09:46 -0500

    edit model for machines, add model for videos(#39)
    * preparing for video table column update
    
    * modified:   checkIn/db_changes.txt
    
    * modified:   checkIn/db_changes.txt
    
    * modified:   checkIn/db_changes.txt
    	modified:   checkIn/model.py
    
    * machines update
    
    * modified:   checkIn/db_changes.txt
    	modified:   checkIn/model.py
    
    * modified:   checkIn/db_changes.txt
    	modified:   checkIn/model.py

--- checkIn/model.py
+++ checkIn/model.py
@@ -302,6 +302,11 @@ class Machine(_base):
 	quiz_issue_days = sa.Column(sa.Integer, nullable=True)
 	quiz_grace_period_days = sa.Column(sa.Integer, nullable=True)
 
+	parent_id = sa.Column(sa.varChar(200), nullable = True)
+	video_id = sa.Column(sa.varChar(200), nullable = False)
+	in_person_component = sa.Column(sa.Boolean, nullable = False)
+	about_link = sa.Column(sa.varChar(100), nullable = True)
+
 	location = relationship('Location')
 	trained_users = relationship('Training')
 	quiz = relationship('Quiz', lazy='joined')
@@ -450,6 +455,13 @@ class TypeInfo:
 		self.level = type.level
 		self.name = type.name
 
+class Video(_base):
+	__tablename__ = 'video'
+	id = sa.Column(sa.Integer, primary_key=True, autoincrement=True, nullable=False)
+	length = sa.Column(sa.Integer, nullable=False)
+	filepath = sa.Column(sa.Text, nullable = False)
+	name = sa.Column(sa.varChar(100), nullable=True)
+	descrip = sa.Column(sa.Text, nullable=True)
 
 def get_types(db) -> Tuple[TypeInfo, TypeInfo]:
 	global ban_type, default_type

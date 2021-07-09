c8126932292c3a46e500d63b66cb3eb80d0e4807
alirpJ <jprila@hawk.iit.edu>
2021-07-09 08:35:36 -0500

    modified:   checkIn/model.py

--- checkIn/model.py
+++ checkIn/model.py
@@ -302,10 +302,10 @@ class Machine(_base):
 	quiz_issue_days = sa.Column(sa.Integer, nullable=True)
 	quiz_grace_period_days = sa.Column(sa.Integer, nullable=True)
 
-	parent_id = sa.Column(sa.varChar(200), nullable = True)
-	video_id = sa.Column(sa.varChar(200), nullable = False)
+	parent_id = sa.Column(sa.VARCHAR(200), nullable = True)
+	video_id = sa.Column(sa.VARCHAR(200), nullable = False)
 	in_person_component = sa.Column(sa.Boolean, nullable = False)
-	about_link = sa.Column(sa.varChar(100), nullable = True)
+	about_link = sa.Column(sa.VARCHAR(100), nullable = True)
 
 	location = relationship('Location')
 	trained_users = relationship('Training')
@@ -460,13 +460,13 @@ class Video(_base):
 	id = sa.Column(sa.Integer, primary_key=True, autoincrement=True, nullable=False)
 	length = sa.Column(sa.Integer, nullable=False)
 	filepath = sa.Column(sa.Text, nullable = False)
-	name = sa.Column(sa.varChar(100), nullable=True)
+	name = sa.Column(sa.VARCHAR(100), nullable=True)
 	descrip = sa.Column(sa.Text, nullable=True)
 
 class Energizer(_base):
 	__tablename__ = 'energizer'
 	id = sa.Column(sa.Integer, primary_key=True, autoincrement=True, nullable=False)
-	name = sa.Column(sa.varChar(50),nullable=False)
+	name = sa.Column(sa.VARCHAR(50),nullable=False)
 	status = sa.Column(sa.Integer,nullable=True)
 	timestamp = sa.Column(sa.DateTime)
 	machine_enabled = sa.Column(sa.Boolean)
